d0d088789380cfe82ca750324fc71dd34fcc88a3
playpool513 <playpool513@gmail.com>
2016-06-13 23:09:21 +0800

    add user_id to Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613142819) do
+ActiveRecord::Schema.define(version: 20160613150817) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160613142819) do
     t.string   "title",      limit: 255
     t.text     "content",    limit: 65535
     t.integer  "group_id",   limit: 4
+    t.integer  "user_id",    limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
