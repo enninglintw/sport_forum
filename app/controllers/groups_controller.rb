295f4014aff974508bb7ab9e4345bda7d79bb3ae
playpool513 <playpool513@gmail.com>
2016-06-13 23:59:37 +0800

    implementation of groups#new/create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @groups = Group.all
@@ -7,6 +8,27 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @posts = @group.posts
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @user = current_user
+    @group = @user.groups.new(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :description)
   end
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   belongs_to :user
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
