f6ecacde4cbcffa97c73dd6aa995cbace774766c
playpool513 <playpool513@gmail.com>
2016-06-14 22:01:25 +0800

    implementation of posts#new/create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,40 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :member_required, only: [:new, :create ]
 
   def show
     @post = Post.find(params[:id])
     @group = @post.group
+  end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def member_required
+    @group = Group.find(params[:group_id])
+    return if current_user.is_member_of?(@group)
+
+    redirect_to group_path(@group)
   end
 
 end
