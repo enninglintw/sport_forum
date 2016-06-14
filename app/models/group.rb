4ed6584489d25f08af5027e142c6bb9924555233
playpool513 <playpool513@gmail.com>
2016-06-14 20:48:31 +0800

    build relationship between GroupUser, Group & User

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,8 @@
 class Group < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   belongs_to :user
+  has_many :group_users, dependent: :destroy
+  has_many :members, through: :group_users, source: :user
 
   validates_presence_of :name
   validates_uniqueness_of :name
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,2 +1,6 @@
 class GroupUser < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :user
+
+  validates_presence_of :group_id, :user_id
 end
